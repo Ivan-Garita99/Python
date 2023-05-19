2c1da08f7630a40b3d62d148c64f8a6695331f4e
José Iván Garita Lobato <ivangarita1999@hotmail.com>
2023-05-19 14:55:11 -0600

    Comentarios Float, False, True

--- tipos/01-variables.py
+++ tipos/01-variables.py
@@ -2,4 +2,7 @@ nombre_curso = "Ultimate Python"
 nombre1 = "Hola"
 NOMBRE_CURSO = "Mundo"
 NombreCurso = "Feliz"
-print(nombre_curso)
+print(nombre_curso, nombre1, NOMBRE_CURSO, NombreCurso)
+alumnos = 5000  # Integer -> Entero
+puntaje = 9.9  # Float
+publicado = True  # valor boolean
