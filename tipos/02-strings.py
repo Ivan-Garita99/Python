2a4acdd8b57476ac1d681905ff2774291ccff66b
José Iván Garita Lobato <ivangarita1999@hotmail.com>
2023-05-19 15:05:50 -0600

    strings y len

---
+++ tipos/02-strings.py
@@ -0,0 +1,8 @@
+nombre_curso = "Ultimate Python"
+descripcion_curso = """
+Ultmate Python,
+este curso abarca los detalles 
+que se necesitan para un trabajo 
+como programador.
+"""
+print(nombre_curso, descripcion_curso)
